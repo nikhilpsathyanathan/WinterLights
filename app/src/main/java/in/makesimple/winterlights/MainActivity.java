ad431aa7022dc28bec2223f652fbe05205d29ab1
Nikhil ps <nikhilpsathyanathan@gmail.com>
2018-10-26 13:19:38 +0530

    initial push

---
+++ app/src/main/java/in/makesimple/winterlights/MainActivity.java
@@ -0,0 +1,237 @@
+package in.makesimple.winterlights;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.macroyau.blue2serial.BluetoothDeviceListDialog;
+import com.macroyau.blue2serial.BluetoothSerial;
+import com.macroyau.blue2serial.BluetoothSerialListener;
+
+import io.feeeei.circleseekbar.CircleSeekBar;
+
+public class MainActivity extends AppCompatActivity implements BluetoothSerialListener, BluetoothDeviceListDialog.OnDeviceSelectedListener {
+
+
+    private static final int REQUEST_ENABLE_BLUETOOTH = 1;
+    static String val = "";
+    private BluetoothSerial bluetoothSerial;
+
+    Button SENSE, END;
+    TextView VALUE, OUT, TV1, TV2;
+    ImageView IMG1, IMG2;
+
+    private MenuItem actionConnect, actionDisconnect;
+    private CircleSeekBar mSeekbar;
+    private boolean crlf = false;
+    int flag = 0;
+    int l = 0, b = 0, h = 0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bluetoothSerial = new BluetoothSerial(this, this);
+
+        mSeekbar = (CircleSeekBar) findViewById(R.id.seekbar);
+
+        mSeekbar.setOnSeekBarChangeListener(new CircleSeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onChanged(CircleSeekBar seekbar, int curValue) {
+                Toast.makeText(getApplicationContext(),curValue+"",Toast.LENGTH_SHORT).show();
+                bluetoothSerial.write("1");
+            }
+        });
+
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        bluetoothSerial.setup();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (bluetoothSerial.checkBluetooth() && bluetoothSerial.isBluetoothEnabled()) {
+            if (!bluetoothSerial.isConnected()) {
+                bluetoothSerial.start();
+            }
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        bluetoothSerial.stop();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_terminal, menu);
+
+        actionConnect = menu.findItem(R.id.action_connect);
+        actionDisconnect = menu.findItem(R.id.action_disconnect);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_connect) {
+            showDeviceListDialog();
+            return true;
+        } else if (id == R.id.action_disconnect) {
+            bluetoothSerial.stop();
+            return true;
+        } else if (id == R.id.action_crlf) {
+            crlf = !item.isChecked();
+            item.setChecked(crlf);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void invalidateOptionsMenu() {
+        if (bluetoothSerial == null)
+            return;
+
+        if (bluetoothSerial.isConnected()) {
+            if (actionConnect != null)
+                actionConnect.setVisible(false);
+            if (actionDisconnect != null)
+                actionDisconnect.setVisible(true);
+        } else {
+            if (actionConnect != null)
+                actionConnect.setVisible(true);
+            if (actionDisconnect != null)
+                actionDisconnect.setVisible(false);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQUEST_ENABLE_BLUETOOTH:
+                if (resultCode == Activity.RESULT_OK) {
+                    bluetoothSerial.setup();
+                }
+                break;
+        }
+    }
+
+    private void updateBluetoothState() {
+        final int state;
+        if (bluetoothSerial != null)
+            state = bluetoothSerial.getState();
+        else
+            state = BluetoothSerial.STATE_DISCONNECTED;
+
+        String subtitle;
+        switch (state) {
+            case BluetoothSerial.STATE_CONNECTING:
+                subtitle = getString(R.string.status_connecting);
+                break;
+            case BluetoothSerial.STATE_CONNECTED:
+                subtitle = getString(R.string.status_connected, bluetoothSerial.getConnectedDeviceName());
+                break;
+            default:
+                subtitle = getString(R.string.status_disconnected);
+                break;
+        }
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setSubtitle(subtitle);
+        }
+    }
+
+    private void showDeviceListDialog() {
+        BluetoothDeviceListDialog dialog = new BluetoothDeviceListDialog(this);
+        dialog.setOnDeviceSelectedListener(this);
+        dialog.setTitle(R.string.paired_devices);
+        dialog.setDevices(bluetoothSerial.getPairedDevices());
+        dialog.showAddress(true);
+        dialog.show();
+    }
+
+
+    @Override
+    public void onBluetoothNotSupported() {
+        new AlertDialog.Builder(this)
+                .setMessage(R.string.no_bluetooth)
+                .setPositiveButton(R.string.action_quit, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                })
+                .setCancelable(false)
+                .show();
+    }
+
+    @Override
+    public void onBluetoothDisabled() {
+        Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        startActivityForResult(enableBluetooth, REQUEST_ENABLE_BLUETOOTH);
+    }
+
+    @Override
+    public void onBluetoothDeviceDisconnected() {
+        invalidateOptionsMenu();
+        updateBluetoothState();
+    }
+
+    @Override
+    public void onConnectingBluetoothDevice() {
+        updateBluetoothState();
+    }
+
+    @Override
+    public void onBluetoothDeviceConnected(String name, String address) {
+        invalidateOptionsMenu();
+        updateBluetoothState();
+    }
+
+    @Override
+    public void onBluetoothSerialRead(String message) {
+
+    }
+
+    @Override
+    public void onBluetoothSerialWrite(String message) {
+
+    }
+
+
+    @Override
+    public void onBluetoothDeviceSelected(BluetoothDevice device) {
+        // Connect to the selected remote Bluetooth device
+        bluetoothSerial.connect(device);
+    }
+}
